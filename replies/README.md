2fec01e629d3a8cfacef8c211115ccf11421f00b
warcode <gaute@warcode.net>
2013-05-07 21:24:51 +0200

    Updated with latest replies plugin from @drewww
    Changed to follow the new event scheme.

--- replies/README.md
+++ replies/README.md
@@ -1,6 +1,10 @@
 # Reply Highlighting
 
-To better support conversations in high-activity rooms, this plugin highlights any message that contains "@yourusername"
+To better support conversations in high-activity rooms, this plugin adds support for @username replies. Any message that contains @yourusername will be highlighted, as will any outgoing messages you send @otherusers. This makes it easier to see messages related to conversations that you're involved in.
+
+The plugin provides two other minor quality of life improvements for conversations. Mousing over a user's name in the message pane will highlight all that user's messages. Clicking on a user's name in the message pane will add @thatusersname to your chat input field, making it easy to address a message to them.
+
+It is perhaps a little bit beyond the purview of a conversation-oriented plugin, but there are a few tools included in this package to make it easier for moderators to be heard in the midst of large conversations. Any user with the moderator role will have their a very light purple background on their chat messages. Any message from a moderator like "QUESTION: " or "TOPIC: " will have a darker background and a border. Any word preceding the colon is fine - but it must be an all caps word followed by a colon. This makes it easier for moderators to mark transition points in a conversation and guide participants in a new direction by visually getting their attention.
 
 ## Usage
 
@@ -12,3 +16,19 @@ To better support conversations in high-activity rooms, this plugin highlights a
 ```JavaScript
 CandyShop.Replies.init();
 ```
+
+The default behavior for this plugin is to override the onClick behavior for names in the chat transcript. Instead of bringing up a private message window, it will instead copy that user's name into your input field for easy @reply-ing. If you would prefer that it not do this, you can initialize it like so:
+
+```JavaScipt
+CandyShop.Replies.init({clickToReply:false});
+```` 
+
+
+## Known Issues
+
+At the moment, there is an issue with usernames longer than about 12 characters. Those usernames get truncated in the message pane, which breaks the click-to-@reply feature, and makes it difficult for users to know the full usernames of others in the room without checking the roster.
+
+There is a fix for this issue here: 
+https://github.com/drewww/candy/tree/message-metadata 
+
+Hopefully it will be merged into the mainline master soon. In the interim, if this functionality is important to you, I recommend using my candy branch or encouraging users to use relatively concise names.
--- replies/candy.js
+++ replies/candy.js
@@ -1,35 +1,136 @@
 /*
- * candy-replies-plugin
- * @version 0.1 (2013-2-20)
- * @author Drew Harry (drew.harry@gmail.com)
- *
- * Adds @reply highlighting to chat messages to help with high velocity 
- * conversations.
- */
+* candy-replies-plugin
+* @version 0.1 (2013-2-20)
+* @author Drew Harry (drew.harry@gmail.com)
+*
+* Adds @reply highlighting to chat messages to help with high velocity 
+* conversations.
+*/
 
 var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.Replies = (function(self, Candy, $) {
 
-	self.init = function() {
-	  
-    Candy.View.Event.Message.onShow = handleOnShow;
-    
-	  return self;
-  };
-  
-  var handleOnShow = function(args) {
-    var localNick = Candy.Core.getUser().getNick();
-    
-    var re = new RegExp("@" + localNick + "([ .!><\":\/@-]|$)", 'im');
-    
-    if(re.test(args.message)) {
-      var el = args.element;
+  self.init = function(options) {
+    if(typeof options === "undefined") {
+      options = {};
+    }
 
+    if("clickToReply" in options) {
+      if(options["clickToReply"]==true){
+        clickToReply = true;
+      } else {
+        clickToReply = false;
+      }
+    }
+
+    $(Candy.View.Pane).on('candy:view.message.after-show', handleOnShow);
+
+    return self;
+  };
+
+  var clickToReply = true;
+
+  var handleOnShow = function(args) {
+    // if the end of the nick has spaces (which, surprisingly, is possible)
+    // trim() those off because it causes drama with the regex.
+    var localNick = Candy.Core.getUser().getNick().toLowerCase().trim();
+
+    var re = new RegExp("@" + localNick + "([ .!><\":\/@-]|$)");
+    var el = args.element;
+
+    if(re.test(args.message.toLowerCase())) {
       el.addClass("mention");
       el.prev().addClass("mention");
     }
+
+    // lets also highlight messages that are FROM the local user, TO anyone.
+    // this makes highlighting function as "all messages in conversations 
+    // i'm involved in" rather than "messages to me" which feels more natural.
+    if(args.nick==localNick) {
+      // now the trick is we need to test for it having an @ message in it.
+      // for now we're just going to look for an @ message somewhere in the
+      // message. This has one weakness: any message that includes an email
+      // address (or anything with an @) but practically, in the conversations
+      // I've seen, that's quite quite rare compared to the former case.
+      if(args.message.indexOf("@")!=-1) {
+        el.addClass("mention");
+        el.prev().addClass("mention");
+      }
+    }
+
+    if(clickToReply) {
+      // now swap in different event handlers for clicking
+      // people's names.
+      el.find("a.name").unbind();
+
+      el.find("a.name").click(function(e) {
+        var inputEl = $(".message-form input[type=text]");
+
+        var fullName = $(this).attr("data-nick");
+
+        // this is a hedge so the plugin continues to work until 
+        // https://github.com/candy-chat/candy/pull/154
+        // is merged onto master. 
+        if(fullName === undefined) {
+          fullName = $(this).text();
+        }
+
+        var addText = "@" + fullName + " ";
+
+        // prepend a space if there's already content there.
+        if(inputEl.val().length > 0 &&
+        inputEl.val()[inputEl.val().length-1] != " "){
+          addText = " " + addText;
+        }
+
+        inputEl.val(inputEl.val() + addText);
+        inputEl.focus();
+      });
+    }
+    
+    var nameEl = el.find("a.name");
+    
+    if(nameEl.attr("data-role")=="moderator") {
+
+      // lets look and see if this message fits the format
+      // WORD: blah blah blah
+      // we'll accept any WORD, and the notion is that moderators
+      // can use it to do something like QUESTION: Are you happy?
+      
+      var alert = new RegExp("[A-Z]{3}:");
+      
+      // if it's an alert, that gets priority over mod status.
+      if(alert.test(args.message)) {
+        nameEl.parent().parent().addClass("alert");
+        nameEl.parent().parent().prev().addClass("alert");
+      } else {
+        nameEl.parent().parent().addClass("mod");
+        nameEl.parent().parent().prev().addClass("mod");
+      }
+    }
+    
+    // on mouseover, highlight all the messages from that user
+    nameEl.mouseenter(function(e) {
+      // if data-nick is available, use that. otherwise, use the text.
+      var targetName = $(this).attr("data-nick")===undefined ? $(this).text() : $(this).attr("data-nick");
+
+      var messagePane = $(".message-pane");
+
+      messagePane.find("a.name").each(function(el) {
+        var elName = $(this).attr("data-nick")===undefined ? $(this).text() : $(this).attr("data-nick");
+
+        if(elName==targetName) {
+          $(this).parent().parent().addClass("user-highlight");
+          $(this).parent().parent().prev().addClass("user-highlight");
+        }
+      });
+    });
+
+    nameEl.mouseleave(function(e) {
+      $(".message-pane .user-highlight").removeClass("user-highlight");
+    });
   }
-  
+
   return self;
-}(CandyShop.Replies || {}, Candy, jQuery));
+  }(CandyShop.Replies || {}, Candy, jQuery));
