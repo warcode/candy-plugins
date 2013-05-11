dc7d9da31c109a89a069af9414aaafc093c5a15b
warcode <gaute@warcode.net>
2013-05-11 12:32:51 -0400

    Requesting vCard on incoming message. Should check against the existing list before requesting.

--- avatar/candy.js
+++ avatar/candy.js
@@ -12,13 +12,21 @@ CandyShop.Avatar = (function(self, Candy, $) {
     
     self.init = function(options) {
 
+        $(Candy.View.Pane).on('candy:view.message.before-show', handleBeforeShow);
         $(Candy.View.Pane).on('candy:view.message.after-show', handleAfterShow);
-        $(Candy.View.Pane).on('view.roster.after-update', handleRosterAfterUpdate);
     };
+
+    var handleBeforeShow = function(e, args) {
+
+        var jid = args.roomJid + '/' + args.name;
+        var realJid = jid.replace(/[\w\d]{1,}[@]\w{1,}[.](\w{1,}[.]\w{2,4})\/([\w -]{1,})/, '\$2@\$1');
+        Candy.Core.getConnection().send($iq({type: 'get', to: realJid, from: Candy.Core.getUser().getJid()}).c('vCard', {xmlns: 'vcard-temp', version: '2.0'}));
+    }
 
     var handleAfterShow = function(e, args) {
 
         var inner = $(args.element).children('div').html();
+	Candy.Core.log(args);
 
         var url = '';
         var customData = Candy.Core.getUser().getCustomData();
@@ -26,20 +34,14 @@ CandyShop.Avatar = (function(self, Candy, $) {
             url = 'https://deny.io/chat/default.jpg';
         }
         else {
-            url = 'data:image/png;base64,' + customData['avatar'][args.name+'@deny.io'];
+            url = 'data:image/png;base64,' + customData['avatar'][args.name.toLowerCase()+'@deny.io'];
             Candy.Core.log(args.message);
         }
 
         var width = 25;
         var height = 25;
         $(args.element).children('div').html('<span class="avatar"><img src="' + url + '" width="' + width + '" height="' + height + '"/></span>' + inner);
-  };
-
-  var handleRosterAfterUpdate = function(e, args) {
-      Candy.Core.log(args.user);
-      Candy.Core.getConnection().send($iq({type: 'get', to: args.user + '@deny.io', from: Candy.Core.getUser().getJid()}).c('vCard', {xmlns: 'vcard-temp', version: '2.0'}));
-
-  };
+    }
 
   return self;
 }(CandyShop.Replies || {}, Candy, jQuery));
