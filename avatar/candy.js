60b26ac0f70927d6576a6b51a9b1557e4becf6e1
warcode <gaute@warcode.net>
2013-05-11 16:50:05 +0200

    Cleanup

--- avatar/candy.js
+++ avatar/candy.js
@@ -9,35 +9,36 @@
 var CandyShop = (function(self) { return self; }(CandyShop || {}));
 
 CandyShop.Avatar = (function(self, Candy, $) {
+    
+    self.init = function(options) {
 
-  self.init = function(options) {
+        $(Candy.View.Pane).on('candy:view.message.after-show', handleAfterShow);
+        $(Candy.View.Pane).on('view.roster.after-update', handleRosterAfterUpdate);
+    };
 
-	$(Candy.View.Pane).on('candy:view.message.after-show', handleAfterShow);
-	$(Candy.View.Pane).on('view.roster.after-update', handleRosterAfterUpdate);
-  };
+    var handleAfterShow = function(e, args) {
 
-  var handleAfterShow = function(e, args) {
+        var inner = $(args.element).children('div').html();
 
-	var inner = $(args.element).children('div').html();
+        var url = '';
+        var customData = Candy.Core.getUser().getCustomData();
+        if(customData['avatar'] == null) {
+            url = 'https://deny.io/chat/default.jpg';
+        }
+        else {
+            url = 'data:image/png;base64,' + customData['avatar'][args.name+'@deny.io'];
+            Candy.Core.log(args.message);
+        }
 
-	var url = '';
-	var customData = Candy.Core.getUser().getCustomData();
-    if(customData['avatar'] == null) {
-        url = 'https://deny.io/chat/default.jpg';
-    }
-    else {
-    	url = 'data:image/png;base64,' + customData['avatar'][args.name+'@deny.io'];
-    	Candy.Core.log(args.message);
-    }
-	var width = 25;
-	var height = 25;
-	$(args.element).children('div').html('<span class="avatar"><img src="' + url + '" width="' + width + '" height="' + height + '"/></span>' + inner);
+        var width = 25;
+        var height = 25;
+        $(args.element).children('div').html('<span class="avatar"><img src="' + url + '" width="' + width + '" height="' + height + '"/></span>' + inner);
   };
 
   var handleRosterAfterUpdate = function(e, args) {
-	Candy.Core.log(args.user);
-	Candy.Core.getConnection().send($iq({type: 'get', to: args.user + '@deny.io', from: Candy.Core.getUser().getJid()}).c('vCard', {xmlns: 'vcard-temp', version: '2.0'}));
-	//, id: '2490'
+      Candy.Core.log(args.user);
+      Candy.Core.getConnection().send($iq({type: 'get', to: args.user + '@deny.io', from: Candy.Core.getUser().getJid()}).c('vCard', {xmlns: 'vcard-temp', version: '2.0'}));
+
   };
 
   return self;
