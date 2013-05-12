ce58debe4e11532206f9b1d6f3cbacb138208e1d
warcode <gaute@warcode.net>
2013-05-12 16:03:11 +0200

    Autofocus Plugin
    This plugin puts the focus on the entry box if the user clicks in the
    message window.

---
+++ autofocus/candy.js
@@ -0,0 +1,23 @@
+/*
+* candy-autofocus-plugin
+* @version 0.1 (2013-5-8)
+* @author warcode (github.com/warcode)
+*
+* This plugin puts the focus on the entry box if the user clicks in the message window.
+*/
+
+var CandyShop = (function(self) { return self; }(CandyShop || {}));
+
+CandyShop.Autofocus = (function(self, Candy, $) {
+    
+    self.init = function(options) {
+
+        $('.message-pane-wrapper').mousedown(function() {
+            $('.message-form').children(".field")[0].focus();
+            return false;
+        });
+       
+    };
+
+  return self;
+}(CandyShop.Replies || {}, Candy, jQuery));
